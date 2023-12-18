f871d02e64192f62246135db07406ae71ecf2df4
Ben Muthalaly <benmuthalaly@gmail.com>
2023-12-18 00:44:21 -0600

    Remove loops for chatting

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -32,21 +30,30 @@ func server() {
 
 	fmt.Printf("Server is listening for connections on port %s...\n", ServerPort)
 
-	for {
-		fmt.Printf("waiting and listening!\n")
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Printf("Error accepting connection: %s\n", err.Error())
-			continue
-		}
-
-		go handleConnection(conn)
+	fmt.Printf("waiting and listening!\n")
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Printf("Error accepting connection: %s\n", err.Error())
+		return
 	}
+	defer conn.Close()
+	fmt.Println("connected!")
+
+	b := make([]byte, 100)
+
+	n, err := conn.Read(b)
+	if err != nil {
+		fmt.Printf("failed to read from connection\n")
+	}
+	fmt.Printf("%d bytes read\n", n)
+	fmt.Printf("message: %s\n", b)
 }
 
 func client() {
-	guru_ip := os.Getenv("MY_IP")
-	addr, err := net.ResolveTCPAddr("tcp", guru_ip+":"+ServerPort)
+	// target_ip := os.Getenv("MY_IP")
+	target_ip := "127.0.0.1"
+	fmt.Println("IP: ", target_ip)
+	addr, err := net.ResolveTCPAddr("tcp", target_ip+":"+ServerPort)
 	if err != nil {
 		log.Fatalf("couldn't resolve server IP: %s", addr)
 	}
@@ -61,40 +68,4 @@ func client() {
 		log.Fatalf("couldn't write to connection")
 	}
 	fmt.Printf("%v bytes written\n", n)
-
-	go handleConnection(conn)
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	fmt.Printf("Connected to: %s\n", conn.RemoteAddr().String())
-	reader := bufio.NewReader(conn)
-	writer := bufio.NewWriter(conn)
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		message, err := reader.ReadString('\n')
-		if err == io.EOF {
-			fmt.Printf("Client at %s has disconnected.\n", conn.RemoteAddr().String())
-			break
-		}
-		if err != nil {
-			log.Printf("Error reading message: %s\n", err.Error())
-			break
-		}
-
-		message = message[:len(message)-1]
-		fmt.Printf("Client: %s\n", message)
-
-		fmt.Print("You: ")
-		scanner.Scan()
-		reply := scanner.Text() + "\n"
-
-		if _, err := writer.WriteString(reply); err != nil {
-			log.Printf("Error sending reply: %s\n", err.Error())
-			break
-		}
-		writer.Flush()
-	}
 }
